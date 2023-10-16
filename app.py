06097b79d673c88247ffbd50048a2f0ca4bbc932
aviathota <aviathota@gmail.com>
2023-10-16 00:47:18 -0400

    whole lotta shit lol

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+page = 0
+
 @app.route("/")
 def home():
+    return render_template('main.html')
+
+@app.route('/rules', methods=['POST'])
+def rules():
+    print("Rules button clicked!")
+    return render_template('rules.html')
+
+@app.route('/home', methods=['POST'])
+def back():
+    print("Back button clicked!")
     return render_template('main.html')
